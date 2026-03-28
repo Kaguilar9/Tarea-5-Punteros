d3ded2dff2f28bd957797e99f14112370057663a
Kaguilar9 <kaguilarm9@miumg.edu.gt>
2026-03-27 19:49:32 -0600

    Video punteros

---
+++ Puntero.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	int edad = 25, *p_edad;
+	p_edad = &edad;
+	
+	cout << "Variable Edad: " << edad << endl;
+	cout << "Puntero edad: " << *p_edad << endl;
+	
+	cout << "--------------Cambiar valores-------------" << endl;
+	*p_edad = 40;
+	
+	cout << "Variable Edad: " << edad << endl;
+	cout << "Puntero edad: " << *p_edad << endl;
+	
+	cout << "--------------Otra edad a *p_edad valores-------------" << endl;
+	int edad2 = 100;
+	p_edad = &edad2;
+	edad = 400;
+	
+	cout << "Variable Edad: " << edad << endl;
+	cout << "Puntero edad: " << *p_edad << endl;
+	cout << "Variable edad2: " << edad2 << endl;
+	
+	system ("pause");
+}
